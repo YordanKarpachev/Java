8d5ede9ac0c47004ba76e4f5d230e1fb13c60c04
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-05-07 00:09:32 +0200

    Add files via upload

---
+++ Java-Advanced-Teil-2-OOP/04.Exercise: Working with Abstraction/A01/Main.java
@@ -0,0 +1,12 @@
+package com.softuni.WorkingWAbstractionExercise.CardSuit_01;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Card Suits:");
+        CardSuits[] cardSuits = CardSuits.values();
+
+        for (CardSuits cardSuit : cardSuits){
+            System.out.printf("Ordinal value: %d; Name value: %s%n", cardSuit.ordinal(), cardSuit);
+        }
+    }
+}

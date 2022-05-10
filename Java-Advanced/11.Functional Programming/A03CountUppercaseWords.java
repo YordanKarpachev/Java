26a1b59d331254d1a04a280e78e3ca9a15c15e0c
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-05-11 00:05:56 +0200

    A03.Count Uppercase Words
    Write a program that reads one line of text from the console. Print the count of words that start with a Uppercase letter, after that print all these words in the same order, like you found them in the text.
    Use a Predicate<String>.

---
+++ Java-Advanced/11.Functional Programming/A03CountUppercaseWords.java
@@ -0,0 +1,21 @@
+package FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class A03CountUppercaseWords {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] strings = scanner.nextLine().split(" ");
+
+        Predicate<String> predicate = str -> Character.isUpperCase(str.charAt(0));
+
+        List<String> upperCaseStrings = Arrays.stream(strings).filter(predicate).collect(Collectors.toList());
+
+        System.out.println(upperCaseStrings.size());
+        System.out.println(String.join(System.lineSeparator(), upperCaseStrings));
+    }
+}

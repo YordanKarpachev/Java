61a76a46dba0ecb4ef6b2cce54075391bc4ebed4
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-05-10 02:22:54 +0200

    A01.Sort Even Numbers
    Write a program that reads one line of Integers separated by ", ". 
        • Print the even numbers.
        • Sort them in ascending order. 
        • Print them again.
    Use 2 Lambda Expresions to do so.

---
+++ Java-Advanced/11.Functional Programming/A01SortEvenNumbers.java
@@ -0,0 +1,31 @@
+package FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class A01SortEvenNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(", "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+
+        List<Integer> evenNumbers = numbers.stream().filter(a -> a % 2 == 0).collect(Collectors.toList());
+
+        Function<Stream<Integer>, String> mutator = x -> x.map(String::valueOf)
+                .collect(Collectors.joining(", "));
+
+        String firstOutput = mutator.apply(evenNumbers.stream());
+
+        System.out.println(firstOutput);
+
+        String secondOutput = mutator.apply(evenNumbers.stream().sorted());
+        System.out.println(secondOutput);
+    }
+}

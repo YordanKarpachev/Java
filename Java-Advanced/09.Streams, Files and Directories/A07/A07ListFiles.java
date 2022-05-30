e44188af648e800f82b087bada11dabe7519600e
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-05-30 16:06:29 +0200

    Add files via upload

--- Java-Advanced/09.Streams, Files and Directories/A07/A07ListFiles.java
+++ Java-Advanced/09.Streams, Files and Directories/A07/A07ListFiles.java
@@ -7,10 +7,12 @@ public class A07ListFiles {
     public static void main(String[] args) {
         File root = new File("C:\\Users\\yorda\\Desktop\\Softuni\\Java Advanced Moq Kurs May 2022\\" +
                 "10.Streams, Files and Directories\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\Files-and-Streams");
-        File[] files = root.listFiles();
 
-        Arrays.stream(files)
+    //root.listFiles()).stream.filter.forEach
+
+
+        Arrays.stream(root.listFiles())
                 .filter(File::isFile)
-                .forEach(a -> System.out.println(a.getName()+": ["+a.length() + "]"));
+                .forEach(a -> System.out.println(a.getName() + ": [" + a.length()+ "]"));
     }
 }

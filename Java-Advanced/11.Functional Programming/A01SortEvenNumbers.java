014a78e781f089973c93169f9121bac8628f22bd
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-06-03 23:06:38 +0200

    Add files via upload

--- Java-Advanced/11.Functional Programming/A01SortEvenNumbers.java
+++ Java-Advanced/11.Functional Programming/A01SortEvenNumbers.java
@@ -1,4 +1,4 @@
-package FunctionalProgramming;
+package FunctionalProgramingLab;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,22 +10,11 @@ import java.util.stream.Stream;
 public class A01SortEvenNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(", "))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-
-
-        List<Integer> evenNumbers = numbers.stream().filter(a -> a % 2 == 0).collect(Collectors.toList());
-
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(", ")).map(Integer::parseInt).collect(Collectors.toList());
+        numbers.removeIf(n -> n % 2 != 0);
         Function<Stream<Integer>, String> mutator = x -> x.map(String::valueOf)
                 .collect(Collectors.joining(", "));
-
-        String firstOutput = mutator.apply(evenNumbers.stream());
-
-        System.out.println(firstOutput);
-
-        String secondOutput = mutator.apply(evenNumbers.stream().sorted());
-        System.out.println(secondOutput);
+        System.out.println(mutator.apply(numbers.stream()));
+        System.out.println(mutator.apply(numbers.stream().sorted()));
     }
 }

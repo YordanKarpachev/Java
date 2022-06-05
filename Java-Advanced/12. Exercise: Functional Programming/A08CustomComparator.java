0742af5b5eb492fa822bbdce810055b6fa21793a
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-06-05 22:29:31 +0200

    Add files via upload

---
+++ Java-Advanced/12. Exercise: Functional Programming/A08CustomComparator.java
@@ -0,0 +1,29 @@
+package FunctionalProgrammingExercise;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class A08CustomComparator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Integer[] input = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
+
+
+        Comparator<Integer> comparator = (num1, num2) -> {
+            if (num1 % 2 == 0 && num2 % 2 != 0) {
+                return -1;
+            } else if (num1 % 2 != 0 && num2 % 2 == 0) {
+                return 1;
+            } else {
+                return
+                num1.compareTo(num2);
+            }
+        };
+        Arrays.sort(input, comparator);
+        Arrays.stream(input).forEach(a -> System.out.print(a + " "));
+    }
+}

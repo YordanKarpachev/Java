9cf9ccc5b8000112027cca56740c1f8fbb986e96
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-06-13 05:45:48 +0200

    Add files via upload

---
+++ Java-Advanced/17.Generics/A04/ListUtils.java
@@ -0,0 +1,22 @@
+package GenericsLAB.A04;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ListUtils {
+
+    public static <T extends Comparable<T>> T getMax(List<T> list) {
+        if (list.size() == 0) {
+        throw  new IllegalArgumentException();
+        }
+
+        return Collections.max(list);
+    }
+
+    public static <T extends Comparable<T>> T getMin(List<T> list){
+        if(list.size() == 0){
+            throw new IllegalArgumentException();
+        }
+        return Collections.min(list);
+    }
+}

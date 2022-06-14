6f2350f6b2e35ea804cd2c9074ddf23bd74fed22
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-06-14 04:05:49 +0200

    Add files via upload

---
+++ Java-Advanced/18.Exercise Generics/A08/Main.java
@@ -0,0 +1,64 @@
+package GenericsExercise.A08;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        CustomList<String> customList = new CustomList<>();
+        while (!input.equals("END")){
+            String command = input.split(" ")[0];
+            String element;
+            int index;
+            int index2;
+
+
+            switch (command){
+                case "Add":
+                     element = input.split(" ")[1];
+
+                    customList.add(element);
+                    break;
+                case "Remove":
+                 index = Integer.parseInt(input.split(" ")[1]);
+                    customList.remove(index);
+
+                    break;
+                case "Contains":
+                    element = input.split(" ")[1];
+                    System.out.println(customList.contains(element));
+                    break;
+                case "Swap":
+                    index = Integer.parseInt(input.split(" ")[1]);
+                    index2 = Integer.parseInt(input.split(" ")[2]);
+                    customList.swap(index, index2);
+                    break;
+                case "Greater":
+                    element = input.split(" ")[1];
+                    System.out.println(customList.countGreaterThan(element));
+                    break;
+                case "Max":
+                    System.out.println(customList.getMax());
+                    break;
+                case "Min":
+                    System.out.println(customList.getMin());
+                    break;
+                case "Sort":
+                    customList.sort();
+                    break;
+                case "Print":
+                    System.out.println(customList.toString());
+                    break;
+
+            }
+
+
+
+            input = scanner.nextLine();
+        }
+
+        System.out.println();
+    }
+}

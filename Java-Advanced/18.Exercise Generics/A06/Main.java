549a2f0dcbc9aec607d92c5b5ea2822d75ff95d4
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-06-14 03:07:30 +0200

    Add files via upload

---
+++ Java-Advanced/18.Exercise Generics/A06/Main.java
@@ -0,0 +1,20 @@
+package A06;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Box<Double> box = new Box<>();
+        for (int i = 0; i < n; i++) {
+           Double input = Double.parseDouble(scanner.nextLine());
+            box.add(input);
+        }
+        Double elementToCompare = Double.parseDouble(scanner.nextLine());
+        System.out.println(box.count(elementToCompare));
+    }
+}

a0a82bcce8a801339e959494ca6f3fe0d9f473e7
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-06-16 01:54:10 +0200

    Add files via upload

---
+++ Java-Advanced/19.IteratorsAndComparators/A01/Book.java
@@ -0,0 +1,46 @@
+package IteratiorsAndComparators.A01Book;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Book {
+    private String title;
+    private int year;
+
+    public Book(String title, int years, String... authors) {
+        this.setTitle(title);
+        this.setYear(years);
+        this.setAuthors(authors);
+    }
+
+    private List<String> authors;
+
+    private String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYears() {
+        return year;
+    }
+
+    private  void setYear(int years) {
+        this.year = years;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    private void setAuthors(String... authors) {
+        if(authors.length == 0){
+          this.authors = new ArrayList<String>();
+
+        } else {
+        this.authors = new ArrayList<>(Arrays.asList(authors));}
+    }
+}

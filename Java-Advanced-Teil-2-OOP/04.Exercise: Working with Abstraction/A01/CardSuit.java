a38493392e40c3db8f45f252e7595123b6ff04a4
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-06-29 14:36:11 +0200

    Add files via upload

---
+++ Java-Advanced-Teil-2-OOP/04.Exercise: Working with Abstraction/A01/CardSuit.java
@@ -0,0 +1,5 @@
+package T04ExercisesWorkingwithAbstraction.A01CardSuits;
+
+public enum CardSuit {
+    CLUBS, DIAMONDS, HEARTS, SPADES;
+}
--- Java-Advanced-Teil-2-OOP/04.Exercise: Working with Abstraction/A01/Main.java
+++ Java-Advanced-Teil-2-OOP/04.Exercise: Working with Abstraction/A01/Main.java
@@ -1,12 +1,12 @@
-package com.softuni.WorkingWAbstractionExercise.CardSuit_01;
+package T04ExercisesWorkingwithAbstraction.A01CardSuits;
 
 public class Main {
     public static void main(String[] args) {
+        CardSuit[] values = CardSuit.values();
         System.out.println("Card Suits:");
-        CardSuits[] cardSuits = CardSuits.values();
 
-        for (CardSuits cardSuit : cardSuits){
-            System.out.printf("Ordinal value: %d; Name value: %s%n", cardSuit.ordinal(), cardSuit);
+        for(CardSuit card : values){
+            System.out.printf("Ordinal value: %d; Name value: %s%n", card.ordinal(), card);
         }
     }
 }

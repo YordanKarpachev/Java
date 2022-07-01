f64e96ab48677695de07c7d68fc0cf9f64fe2dce
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-07-01 09:20:54 +0200

    Add files via upload

---
+++ Java-Advanced-Teil-2-OOP/05.Encapsulation/A02/Person.java
@@ -0,0 +1,62 @@
+package A02;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    private double salary;
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s gets %.1f leva", getFirstName(), getLastName(), getSalary());
+    }
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void increaseSalary(double bonus){
+
+        if(getAge() < 30) {
+            salary += salary * bonus / 200;
+        } else {
+            salary += salary * bonus / 100;
+        }
+    }
+}

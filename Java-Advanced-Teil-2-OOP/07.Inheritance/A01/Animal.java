a594f40e4d1a40892dc3dcd98b32f1389b3c8a5f
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-07-04 00:40:21 +0200

    Add files via upload

---
+++ Java-Advanced-Teil-2-OOP/07.Inheritance/A01/Animal.java
@@ -0,0 +1,8 @@
+package A01;
+
+public class Animal {
+
+    public void eat(){
+        System.out.println("eating…");
+    }
+}
---
+++ Java-Advanced-Teil-2-OOP/07.Inheritance/A01/Dog.java
@@ -0,0 +1,8 @@
+package A01;
+
+public class Dog extends Animal{
+    public void
+    bark() {
+        System.out.println("barking…");
+    }
+}

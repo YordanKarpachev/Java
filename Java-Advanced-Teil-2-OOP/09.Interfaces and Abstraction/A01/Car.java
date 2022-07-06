3c2c42a533ac77730352559fbbef5598d6d96cb4
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-07-06 05:53:53 +0200

    Add files via upload

---
+++ Java-Advanced-Teil-2-OOP/09.Interfaces and Abstraction/A01/Car.java
@@ -0,0 +1,15 @@
+package T09InterfacesandAbstraction.A01;
+
+
+public interface Car  {
+    Integer TIRES = 4;
+
+    String getModel();
+
+    String getColor();
+
+    Integer getHorsePower();
+
+    String countryProduced();
+
+}
---
+++ Java-Advanced-Teil-2-OOP/09.Interfaces and Abstraction/A01/Main.java
@@ -0,0 +1,13 @@
+package T09InterfacesandAbstraction.A01;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Car seat = new Seat("Leon", "gray", 110, "Spain");
+
+
+        System.out.println(seat.toString());
+    }
+
+}

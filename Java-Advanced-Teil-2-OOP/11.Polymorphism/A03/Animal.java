f066c80cac0346ee009bbaa02c12c6c5fc828524
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-07-11 01:16:46 +0200

    Add files via upload

---
+++ Java-Advanced-Teil-2-OOP/11.Polymorphism/A03/Animal.java
@@ -0,0 +1,31 @@
+package A03;
+
+public abstract class Animal {
+    String name;
+    String favouriteFood;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFavouriteFood() {
+        return favouriteFood;
+    }
+
+    public void setFavouriteFood(String favouriteFood) {
+        this.favouriteFood = favouriteFood;
+    }
+
+    public Animal(String name, String favouriteFood) {
+        this.name = name;
+        this.favouriteFood = favouriteFood;
+    }
+
+    public String explainSelf() {
+        return "I am " + this.name + " and my favourite food is " + this.favouriteFood + System.lineSeparator();
+    }
+}
---
+++ Java-Advanced-Teil-2-OOP/11.Polymorphism/A03/Dog.java
@@ -0,0 +1,13 @@
+package A03;
+
+public class Dog extends Animal{
+
+    public Dog(String name, String favoriteFood) {
+       super(name, favoriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return super.explainSelf() + "DJAAF";
+    }
+}

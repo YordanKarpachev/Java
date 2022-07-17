11db981e4923981f0eb91a61827d5e2ba840bca6
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-07-17 03:35:44 +0200

    Add files via upload

---
+++ Java-Advanced-Teil-2-OOP/15.ReflectionAndAnnotation/A05/Author.java
@@ -0,0 +1,14 @@
+package T15.ReflectionAndAnnotation.A05;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Author {
+    String name()
+    default "Unknow";
+}

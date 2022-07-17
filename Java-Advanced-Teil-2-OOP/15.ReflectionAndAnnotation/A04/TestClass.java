4748913d3920060da65a12986b83c228e33831ad
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-07-17 03:34:19 +0200

    Add files via upload

---
+++ Java-Advanced-Teil-2-OOP/15.ReflectionAndAnnotation/A04/TestClass.java
@@ -0,0 +1,13 @@
+package T15.ReflectionAndAnnotation.A04;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+
+
+public @interface TestClass {
+}

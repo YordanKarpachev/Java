f4149df82f89981d4188aea15959dfc0260cdabf
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-07-18 03:01:46 +0200

    Add files via upload

---
+++ Java-Advanced-Teil-2-OOP/16.Exercise Reflection and Annotation/A03/models/units/Horseman.java
@@ -0,0 +1,11 @@
+package barracksWars.models.units;
+
+public class Horseman extends AbstractUnit{
+
+    private static final int Horseman_HEALTH = 50;
+    private static final int Horseman_DAMAGE = 10;
+
+    public Horseman() {
+        super(Horseman_HEALTH, Horseman_DAMAGE);
+    }
+}

032071059419a8145888dfd13a73e3bb4d98e7ba
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-07-20 01:59:21 +0200

    Add files via upload

---
+++ Java-Advanced-Teil-2-OOP/14.Exercise: SOLID/Step 1/CalorieCalculator.java
@@ -0,0 +1,51 @@
+package solid;
+
+import solid.products.Chocolate;
+import solid.products.Coke;
+import solid.products.Lemonade;
+import solid.products.Print;
+
+import java.util.List;
+
+public class CalorieCalculator {
+
+    private static final String SUM_FORMAT = "Sum: %f";
+    private static final String AVERAGE_FORMAT = "Average: %f";
+    Print print;
+
+    public CalorieCalculator() {
+        this.print = new Print();
+    }
+
+    public double sum(List<Object> products) {
+        double sum = 0;
+
+        for (Object product : products) {
+            if (product instanceof Coke) {
+                double grams = ((Coke) product).getMilliliters() * Coke.DENSITY;
+                sum += (Coke.CALORIES_PER_100_GRAMS / 100) * grams;
+            }
+
+            if (product instanceof Lemonade) {
+                double grams = ((Lemonade) product).getMilliliters() * Lemonade.DENSITY;
+                sum += (Lemonade.CALORIES_PER_100_GRAMS / 100) * grams;
+            }
+
+            if (product instanceof Chocolate) {
+                sum += (Chocolate.CALORIES_PER_100_GRAMS / 100) * ((Chocolate) product).getGrams();
+            }
+        }
+
+
+        return sum;
+    }
+
+    public double average(List<Object> products) {
+
+        return sum(products) / products.size();
+    }
+
+    public void print(String format, double sum) {
+        print.print(format, sum);
+    }
+}
---
+++ Java-Advanced-Teil-2-OOP/14.Exercise: SOLID/Step 1/products/Print.java
@@ -0,0 +1,19 @@
+package solid.products;
+
+import java.util.List;
+
+public class Print {
+
+   // public void printSum(List<Object> products) {
+      //  System.out.printf((SUM) + "%n", sum(products));
+  //  }
+
+   // public void printAverage(List<Object> products) {
+       // System.out.printf((AVERAGE) + "%n", average(products));
+   // }
+
+    public void print(String format, double sum){
+        System.out.printf(format, sum);
+
+    }
+}
